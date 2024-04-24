710a5de24cfba40fa6792e6bb13f7b8eb2efa8e8
joao roberto <jrgarske5@gmail.com>
2024-04-24 15:01:25 -0300

    adicionado about.fxml e adicionado method loadView

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,10 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 	try {
@@ -20,7 +24,7 @@ public class Main extends Application {
 	 scrollPane.setFitToWidth(true);
 	 
 	 
-	 Scene mainScene = new Scene(scrollPane);
+	 mainScene = new Scene(scrollPane);
 	 primaryStage.setScene(mainScene);
 	 primaryStage.setTitle("Sample JavaFX application");
 	 primaryStage.show();
@@ -32,5 +36,9 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 }
